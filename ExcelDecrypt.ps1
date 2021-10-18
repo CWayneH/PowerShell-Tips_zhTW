bc14dbb59f5b29255614ac1a2579d6f037ffe529
CWayneH <90815681+CWayneH@users.noreply.github.com>
2021-10-18 09:33:02 +0800

    v1.0
    to decrypt excel files with file path and password input, then move to '\saveDir' which created first at the same directory.

---
+++ ExcelDecrypt.ps1
@@ -0,0 +1,20 @@
+$ExcelFileDir=Read-Host "FileDir"
+$ExcelPWd=Read-Host "Password" -AsSecureString
+$ExcelPWd=[System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($ExcelPWd)
+$ExcelPWd=[System.Runtime.InteropServices.Marshal]::PtrToStringAuto($ExcelPWd)
+$ExcelSaveDir=$ExcelFileDir+'\saveDir\'
+$temp=dir $ExcelFileDir -Name -Include *.xlsx -Exclude IGR*.xlsx,CDC*.xlsx
+$ExcelObj = $null
+$ExcelObj = New-Object -ComObject Excel.Application
+for($i=0;$i-lt$temp.Length;$i++){
+$ExcelWorkBook = $ExcelObj.Workbooks.Open($ExcelFileDir+'\'+$temp[$i],0,0,5,$ExcelPWd)
+Write-Host $temp[$i]
+$ExcelWorkBook.Password = $null
+$ExcelWorkBook.SaveAs($ExcelSaveDir+$temp[$i])
+#$ExcelWorkBook = $null
+$ExcelObj.Workbooks.Close()
+}
+$ExcelObj.Quit()
+[System.Runtime.Interopservices.Marshal]::ReleaseComObject($ExcelObj)
+Remove-Variable $ExcelObj
+pause
