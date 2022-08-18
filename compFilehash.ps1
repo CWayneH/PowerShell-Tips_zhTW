ebee1b963e6f15d3256df11e648f7322b5fa8002
CWayneH <90815681+CWayneH@users.noreply.github.com>
2022-08-18 16:29:14 +0800

    Update compFilehash.ps1
    rewrite with no compress due to file issue

--- compFilehash.ps1
+++ compFilehash.ps1
@@ -1,22 +1,34 @@
 # Author@CWayneH
+# 111/0815 Rewrite w/ no compress 
+# due to tired of handle files issues
 $path=Read-Host "Existing Directory"
-$path_bak=Read-Host "Backup Directory"
-$file=$path+"\backup."+$(Get-Date -f "yyyyMMddHHmmss")+".zip"
-# all existing files need check
-dir -Exclude *.zip,*.neq $path | Compress-Archive -DestinationPath $file -Force
-$recent=Get-FileHash $file -Algorithm "SHA256"
-# where last backup to compare
-$last_bak_file=$path_bak+"\"+(Get-ChildItem -Path $path_bak -Exclude *.log| sort LastWriteTime | select -Last 1).Name
-$last=Get-FileHash $last_bak_file -Algorithm "SHA256"
-# log write into backup directory
-$logFile=$path_bak+"\baklog."+$(Get-Date -f "yyyyMMdd")+".log"
-if($recent.Hash -ne $last.Hash) {
-	Add-Content -Path $logFile -Value $(Get-Date -f "yyyy-MM-dd HH:mm:ss")
-	Add-Content -Path $logFile -Value "Last Backup file Hash Result : $last `ndoes not euqal Existing Destination File Hash Result : $recent `nthe files are NOT EQUAL."
-	Rename-Item -Path $file -NewName ($file+".neq")
-} else {
-	Add-Content -Path $logFile -Value " $(Get-Date -f "yyyy-MM-dd HH:mm:ss") `nfiles are EQUAL. Movement done."
-	Move-Item -Path $file -Destination $path_bak
+$path_prot=Read-Host "Protect Directory"
+$ext=Read-Host "Monitor Filename Extension"
+# if protect folders empty
+if(!(Test-Path $path_prot\*)) {
+	# this will encounter subflolder w/ duplicate files problem; if so, please copy in manual.
+	Get-ChildItem -Path $path -Filter $ext -Recurse | Copy-Item -Destination $path_prot -Force
 }
+# all existing files need check
+$exist=dir -Path $path -Filter $ext -Recurse | Get-FileHash -Algorithm "SHA256"
+# where protect backup to compare
+$prot=dir -Path $path_prot -Filter $ext -Recurse | Get-FileHash -Algorithm "SHA256"
+# compare two string array
+$c=Compare-Object -ReferenceObject $prot.Hash -DifferenceObject $exist.Hash
+$abnormal=($c|where{$_.SideIndicator-eq"=>"}).InputObject
+$path_abn=($exist | where {$abnormal -contains $_.hash}).Path
+# $opt_abn=$("$path_abn".replace(" ","`n"))
+# log write into backup directory
+$path_log=Read-Host "LogFiles Directory"
+$logFile=$path_log+"\baklog."+$(Get-Date -f "yyyyMMdd")+".log"
+if($c -eq $null) {
+	Add-Content -Path $logFile -Value "`t$(Get-Date -f "yyyy-MM-dd HH:mm:ss") `nfiles are EQUAL."
+	# Rename-Item -Path $file -NewName ($file+".neq")
+} else {
+	Add-Content -Path $logFile -Value "`t$(Get-Date -f "yyyy-MM-dd HH:mm:ss")"
+	Add-Content -Path $logFile -Value "Protect Backup files Hash Result Difference: $abnormal `nthe files are NOT EQUAL. `nPlease CHECK the files Below:`n$("$path_abn".replace(" ","`n"))"
+	# Move-Item -Path $file -Destination $path_bak
+}
+Add-Content -Path $logFile -Value "`tDone."
 Write-Host "Done."
 pause
