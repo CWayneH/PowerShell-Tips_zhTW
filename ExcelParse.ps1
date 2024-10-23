401d93b37b1ac7660c2890ada547149e77453f98
CWayneH <90815681+CWayneH@users.noreply.github.com>
2024-10-23 23:59:16 +0800

    Update ExcelParse.ps1

--- ExcelParse.ps1
+++ ExcelParse.ps1
@@ -1,12 +1,19 @@
 # Gather shell what you need.
 <#
-match result between asset list and state property
+match result between asset ledger and state property
 #>
-$sheet_list = Get-ChildItem -Name *.xls
+$ExcelFileDir=Read-Host "FileDir"
+# $temp=@(dir $ExcelFileDir -Name -Include *.xls)
 $ExcelObj = New-Object -ComObject Excel.Application
-$ExcelWorkBook = $ExcelObj.Workbooks.Open('D:\\..\\..\\ASSET.xls')
-$ExcelWorkBook.Sheets.Item(1).range("A:A")
-
+# $ExcelWorkBook = $ExcelObj.Workbooks.Open($ExcelFileDir+'\'+$temp[0])
+$sp_file_name = Read-Host "State Property File Name"
+$ExcelWorkBook = $ExcelObj.Workbooks.Open($ExcelFileDir+'\'+$sp_file_name)
+$sp_list_uni = $ExcelWorkBook.Sheets.Item(1).columns(1).value2
+$al_file_name = Read-Host "Asset Ledger File Name"
+$ExcelWorkBook = $ExcelObj.Workbooks.Open($ExcelFileDir+'\'+$al_file_name)
+$al_list_uni = $ExcelWorkBook.Sheets.Item(1).columns(1).value2
+$res = (Compare-Object -ReferenceObject ($sp_list_uni) -DifferenceObject ($al_list_uni)).InputObject
+$res | Out-File -FilePath $ExcelFileDir -Append} 
 # gc
 $ExcelObj.Workbooks.Close()$ExcelObj.Quit()
 [System.Runtime.Interopservices.Marshal]::ReleaseComObject($ExcelObj)
